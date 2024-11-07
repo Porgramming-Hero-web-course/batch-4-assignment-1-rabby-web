7e76e0af91d6d135fa0a0c15d5583fb885a1bdb4
rabby-web <rabby.webdeveloper@gmail.com>
2024-11-07 16:24:55 +0600

    problem-1.ts solve

--- src/problem-1.ts
+++ src/problem-1.ts
@@ -1,2 +1,8 @@
-const myNum: number = 333;
-console.log(myNum);
+function sumArray(arrNumbers: number[]): number {
+  return arrNumbers.reduce(
+    (previousNumber, currentNumber) => previousNumber + currentNumber,
+    0
+  );
+}
+const result: number = sumArray([1, 2, 3, 4, 5]);
+console.log(result);
