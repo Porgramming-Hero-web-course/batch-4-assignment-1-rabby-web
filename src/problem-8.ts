211f0690714999c09e18d91ff528a3ebe4186906
rabby-web <rabby.webdeveloper@gmail.com>
2024-11-07 21:23:47 +0600

    problem-8.ts solve

---
+++ src/problem-8.ts
@@ -0,0 +1,10 @@
+{
+  function validateKeys<T>(obj: T, keys: (keyof T)[]): boolean {
+    return keys.every((key) => key in obj);
+  }
+
+  // Sample Input
+  const person = { name: "Alice", age: 25, email: "alice@example.com" };
+  console.log(validateKeys(person, ["name", "age"]));
+  console.log(validateKeys(person, ["name", "gender"]));
+}
